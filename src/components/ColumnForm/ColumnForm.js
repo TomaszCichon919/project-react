9c8a2f2bc404ea1d0093776e01cbcfc3774c8626
Tomasz Cichon <tomekcichon@tlen.pl>
2023-08-14 15:13:22 +0200

    add columns

---
+++ src/components/ColumnForm/ColumnForm.js
@@ -0,0 +1,31 @@
+import styles from './ColumnForm.module.scss';
+import { useState } from 'react';
+import Button from '../Button/Button';
+import TextInput from '../TextInput/TextInput'
+
+const ColumnForm = props => {
+    const [title, setTitle] = useState('');
+    const [icon, setIcon] = useState('');
+    const handleSubmit = e => {
+        e.preventDefault();
+        props.action({ title: title, icon: icon });
+        setTitle('');
+        setIcon('');
+};
+
+	return (
+        <form onSubmit={handleSubmit} className={styles.columnForm}>
+            <span className={styles.textField}>Title:</span>
+             <TextInput placeholder="title..." value={title} onChange={e => setTitle(e.target.value)}>
+             </TextInput>
+             <span className={styles.textField}>Icon:</span>
+             <TextInput placeholder="icon..." value={icon} onChange={e => setIcon(e.target.value)}>
+             </TextInput>
+           {/* Title: <input type="text" value={title} onChange={e => setTitle(e.target.value)} /> */}
+            {/* Icon: <input type="text" value={icon} onChange={e => setIcon(e.target.value)} /> */} 
+            <Button>Add column</Button>
+        </form>
+	);
+};
+
+export default ColumnForm;
